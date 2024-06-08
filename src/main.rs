7c1870e26cca74636e8549f776bd994b939bcad0
Jacob <jacubeyeetmc@gmail.com>
2024-06-08 01:09:54 -0600

    Started text file functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::f64;
+use std::fs;
 
 fn main() {
     // Get the radius of the circle
@@ -7,11 +8,9 @@ fn main() {
 
     let mut r = String::new();
 
-    io::stdin()
-        .read_line(&mut r)
-        .expect("Failed tp read line");
+    io::stdin().read_line(&mut r).expect("Failed to read line");
 
-    let r: f64 = r.trim().parse().expect("Enter a floating point integer.");
+    let r: f64 = r.trim().parse().expect("Enter a floating point integer");
     let r = r.abs();
 
     // Get the interval (spacing) for each point
@@ -19,11 +18,9 @@ fn main() {
 
     let mut i = String::new();
 
-    io::stdin()
-        .read_line(&mut i)
-        .expect("Failed tp read line");
+    io::stdin().read_line(&mut i).expect("Failed to read line");
 
-    let i: f64 = i.trim().parse().expect("Enter an integer.");
+    let i: f64 = i.trim().parse().expect("Enter an integer");
     let i = i.abs().floor();
 
     println!("");
@@ -34,7 +31,23 @@ fn main() {
     let mut a = 0.0;
     let mut stand: u64 = 0;
 
-    // While loop for each point and command
+    // Text file
+    println!("Do you want to output to a text file? (y/n)");
+    loop {
+        let mut ans = String::new();
+        io::stdin().read_line(&mut ans).expect("Failed to read line");
+
+        match ans.as_str().trim() {
+            "y" | "Y" => {
+                text();
+                break();
+            },
+            "n" | "N" => break,
+            _ => println!("y/n only"),
+        }
+    }
+
+    // While loop for each point
     while c != i {
         println!("{:.8}, {:.8}", r * f64::cos(a), r * f64::sin(a));
         println!("execute at @e[tag=stand] run tp @e[tag=stand{stand}] ^{:.4} ^ ^{:.4} facing entity @e[tag=stand,limit=1]\n", r * f64::cos(a), r * f64::sin(a));
@@ -46,4 +59,10 @@ fn main() {
     
     println!("Press Enter to exit...");
     io::stdin().read_line(&mut String::new()).unwrap();
+}
+
+fn text() {
+    let text = format!("test");
+    fs::write("text.txt", text).expect("Unable to write to file");
+    println!("Created test.txt in local directory\n");
 }
